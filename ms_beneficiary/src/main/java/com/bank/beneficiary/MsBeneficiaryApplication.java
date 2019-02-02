a05ca7bb26453a6f00573bdeb276d8040049d8a3
Ram <ramsonone21jd@gmail.com>
2019-02-02 16:09:33 +0530

    start of microservice project

---
+++ ms_beneficiary/src/main/java/com/bank/beneficiary/MsBeneficiaryApplication.java
@@ -0,0 +1,14 @@
+package com.bank.beneficiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBeneficiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsBeneficiaryApplication.class, args);
+	}
+
+}
+
---
+++ ms_transfer/src/main/java/com/bank/transfer/MsTransferApplication.java
@@ -0,0 +1,14 @@
+package com.bank.transfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsTransferApplication.class, args);
+	}
+
+}
+
